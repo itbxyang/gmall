e9f679637b24e6e5b964476418fbd8ff08fa9525
bxyang <1870761663@qq.com>
2020-04-03 12:15:22 +0800

    分布式锁及AOP实现缓存

---
+++ gmall-index/src/main/java/com/atguigu/gmall/index/annotation/GmallCache.java
@@ -0,0 +1,26 @@
+package com.atguigu.gmall.index.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface GmallCache {
+    /**
+     * 缓存key的前缀
+     * @return
+     */
+    String prefix() default "";
+
+    /**
+     * 缓存的过期时间，以分为单位
+     * @return
+     */
+    int timeout() default 5;
+
+    /**
+     * 防止缓存雪崩指定的随机值范围
+     * @return
+     */
+    int random() default 5;
+}
